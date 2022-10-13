78affc32c5ad63a0c2eb31820fe05f02de27a557
Tommy Voigt <112673753+TommyVoigt@users.noreply.github.com>
2022-10-13 14:00:47 -0400

    Updating with figma mockup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,38 +1,18 @@
 <template>
-  <Luisa :design="design" :config="config" v-model="viewModel"/>
+  <Luisa :design="design" v-model="viewModel"/>
+
 </template>
+
 <script>
-/**
- * Here is the downloaded figma file. To download yours, type in the command line
- * node download.js.
- */
-import app from './app.json'
-
-/**
- * Import mdi fonts if you are using quant-ux designs
- */
-//import '@mdi/font/css/materialdesignicons.css'
-
-
 export default {
   name: 'Home',
   data: function() {
-    return {
-      design: app,
-      viewModel: {
-        name:'',
-        result: ''
-      },
-      config: {
+      return {
+          design: {
+              figmaFile: "tDAPugfXQaN8OpQUwXxWfo",
+              figmaAccessKey: "figd_-4avTn_XdIeHrIgixid4vK84abG-76CROzD1iCTf",
+          }
       }
-    }
-  },
-  components: {
-  },
-  methods: {
-    sayHello () {
-      this.viewModel.result = "Hello " + this.viewModel.name
-    }
   }
 }
 </script>
